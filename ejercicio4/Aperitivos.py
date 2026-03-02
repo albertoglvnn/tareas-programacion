a56fd68a795da1417b1492cecf345a3f681e234a
albertoglvnn <al25761077@ite.edu.mx>
2026-03-01 20:10:40 -0800

    Delete ejercicio4 directory

--- ejercicio4/Aperitivos.py
+++
@@ -1,7 +0,0 @@
-from Platillo import Platillo
-
-class Aperitivo (Platillo):
-    def __init__(self,nombre,precio,tamaño):
-        super().__init__(nombre,precio)
-        self.tamaño = tamaño
-        
